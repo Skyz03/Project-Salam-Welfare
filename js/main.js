17af0850d936e08f48a938e1509703b4b1b18d8f
Skyz03 <aakibdiscoo@gmail.com>
2023-07-11 16:57:32 +0545

    update: owl car

--- js/main.js
+++ js/main.js
@@ -99,6 +99,28 @@
         }
     });
 
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        loop: true,
+        navText: [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 3
+            }
+        }
+    })
+
 
 })(jQuery);
 

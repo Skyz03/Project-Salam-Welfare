1448581d521bc64564345df04493ef1a296ae2bd
Skyz03 <aakibdiscoo@gmail.com>
2023-07-11 17:21:51 +0545

    update: base level done

--- js/main.js
+++ js/main.js
@@ -56,28 +56,6 @@
     }, { offset: '80%' });
 
 
-    // Testimonials carousel
-    $(".service-carousel").owlCarousel({
-        autoplay: false,
-        smartSpeed: 1000,
-        center: true,
-        dots: false,
-        loop: true,
-        nav: true,
-        navText: [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-        responsive: {
-            0: {
-                items: 1
-            },
-            768: {
-                items: 2
-            }
-        }
-    });
-
     $(".testimonial-carousel").owlCarousel({
         autoplay: false,
         smartSpeed: 1000,
